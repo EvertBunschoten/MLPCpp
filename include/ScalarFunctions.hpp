cbd05aa2e7adabf0513cdb17a3c583ed16f43b25
EvertBunschoten <evertbun@xs4all.nl>
2026-02-27 13:29:00 +0100

    Commenting on scaler functions

--- include/ScalarFunctions.hpp
+++ include/ScalarFunctions.hpp
@@ -1,3 +1,32 @@
+/*!
+* \file ScalerFunctions.hpp
+* \brief Functions used to scale network input and output.
+* \author E.C.Bunschoten
+* \version 1.2.0
+*
+* MLPCpp Project Website: https://github.com/EvertBunschoten/MLPCpp
+*
+* Copyright (c) 2023 Evert Bunschoten
+
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 #pragma once
 #include <vector>
 #include <iostream>
@@ -14,31 +43,97 @@
 #include "option_maps.hpp"
 
 namespace MLPToolbox {
-class ScalerFunction {
-    protected:
-    size_t n_scalars{0};
-    std::vector<mlpdouble> norm_scalars;
-    std::string tag;
+
+     class ImproperScaleValues: public std::exception 
+    {
+    std::string outp_message;
     public:
-    ScalerFunction(const size_t n_in) : n_scalars {n_in} {norm_scalars.resize(n_scalars);};
-    virtual mlpdouble Normalize(const mlpdouble scalar_dim, const size_t i_scalar) const =0;
-    virtual mlpdouble Dimensionalize(const mlpdouble scalar_dim, const size_t i_scalar) const =0;
-    virtual mlpdouble Distance(const std::vector<mlpdouble>) const = 0;
-    virtual mlpdouble Distance(const mlpdouble*) const = 0;
-    virtual void SetScaling(const size_t, const mlpdouble, const mlpdouble) =0;
-    virtual mlpdouble GetScale(const size_t) const =0;
-    virtual mlpdouble GetOffset(const size_t) const = 0;
-    virtual void PrintInfo(const int display_width, const std::vector<std::string>&) const =0;
-    std::string GetTag() const {return tag;}
+        ImproperScaleValues(const std::string message)
+        {
+            outp_message = message;
+        };
+        virtual const char *what() const noexcept {
+            char *cstr = new char[outp_message.size() + 1];
+            std::strcpy(cstr, outp_message.c_str());
+            return cstr;
+        }
+    };
+
+class ScalerFunction {
+    /*! \brief Base class for scaler functions. */
+    protected:
+    size_t n_scalars{0}; /* Number of variables to scale. */
+    std::string tag;     /* Scaling function ID tag. */
+    public:
+    ScalerFunction(const size_t n_in) : n_scalars {n_in} {};
     virtual ~ScalerFunction() = default;
+
+    /*!
+    * \brief Normalize network input.
+    * \param[in] scalar_dim - Unscaled network input.
+    * \param[in] i_scalar - Network input index.
+    * \returns - Normalized network input.
+    */
+    virtual mlpdouble Normalize(const mlpdouble scalar_dim, const size_t i_scalar) const =0;
+
+    /*!
+    * \brief Unscale network output
+    * \param[in] scalar_norm - Scaled network output.
+    * \param[in] i_scalar - Network output index.
+    * \returns - Unscaled network output.
+    */
+    virtual mlpdouble Dimensionalize(const mlpdouble scalar_norm, const size_t i_scalar) const =0;
+
+    /*!
+    * \brief Calculate normalized distance from feature range
+    * \param[in] inputs - Unscaled network inputs
+    * \returns - Normalized distance.
+    */
+    virtual mlpdouble Distance(const std::vector<mlpdouble> inputs) const = 0;
+    virtual mlpdouble Distance(const mlpdouble* inputs) const = 0;
+
+    /*!
+    * \brief Set linear scaling values.
+    * \param[in] i_scalar - node index.
+    * \param[in] val_1 - first linear scaling value.
+    * \param[in] val_2 - second linear scaling value.
+    */
+    virtual void SetScaling(const size_t i_scalar, const mlpdouble val_1, const mlpdouble val_2) =0;
+
+    /*!
+    * \brief Return data multiplication factor.
+    * \param[in] i_scalar - node index.
+    */
+    virtual mlpdouble GetScale(const size_t i_scalar) const =0;
+
+    /*!
+    * \brief Return data offset.
+    * \param[in] i_scalar - node index.
+    */
+    virtual mlpdouble GetOffset(const size_t i_scaler) const = 0;
+
+    /*!
+    * \brief Display scaler function information in the terminal.
+    * \param[in] display_width - maximum column width.
+    * \param[in] input_names - names of input/output variables.
+    */
+    virtual void PrintInfo(const int display_width, const std::vector<std::string>& input_names) const =0;
+
+    /*!
+    * \brief Get scaler function ID tag.
+    * \returns - tag name.
+    */
+    std::string GetTag() const {return tag;}
 };
 
 
 class StandardScaler : public ScalerFunction {
-    public: 
+    /*! \brief Scaler function using standard deviation. n = (d - mu)/std */
 
-    std::vector<mlpdouble> vals_mu;
-    std::vector<mlpdouble> vals_std;
+    public: 
+    std::vector<mlpdouble> vals_mu;  /*! Mean values */
+    std::vector<mlpdouble> vals_std; /*! Standard deviation values. */
+
     StandardScaler(const size_t n_in) : ScalerFunction(n_in) {
         tag = "standard";
         vals_mu.resize(n_in); 
@@ -51,8 +146,7 @@ class StandardScaler : public ScalerFunction {
     
     virtual void SetScaling(const size_t i_in, const mlpdouble val_mu=0.0, const mlpdouble val_std=1.0) 
     {
-        if (val_std < 0) 
-            std::cout << "Error: standard deviation should be positive" << std::endl;
+        if (val_std < 0) throw ImproperScaleValues(std::string("Error: standard deviation should be positive"));
         vals_mu[i_in] = val_mu;
         vals_std[i_in] = val_std;
     }
@@ -113,6 +207,7 @@ class StandardScaler : public ScalerFunction {
 };
 
 class RobustScaler : public StandardScaler {
+    /*! \brief Inter-quantile range scaling. Similar to standard scaling, but using inter-quantile range rather than standard deviation. */
     public:
     RobustScaler(const size_t n_in) : StandardScaler(n_in) {tag = "robust";};
     virtual void PrintInfo(const int display_width, const std::vector<std::string> &input_names) const {
@@ -143,6 +238,8 @@ class RobustScaler : public StandardScaler {
 };
 
 class MinMaxScaler :  public ScalerFunction {
+    /*! \brief min-max scaler function: n = (d - min)/(max - min). */
+
     std::vector<mlpdouble> vals_min;
     std::vector<mlpdouble> vals_max;
     
@@ -159,8 +256,7 @@ class MinMaxScaler :  public ScalerFunction {
     
     virtual void SetScaling(const size_t i_in, const mlpdouble min=0, const mlpdouble max=1)
     {
-        if (min >= max) 
-            std::cout << "Error: maximum scaling value should be higher than minimum scaling value" << std::endl;
+        if (min >= max) throw ImproperScaleValues(std::string("Error: maximum scaling value should be higher than minimum scaling value"));
         vals_min[i_in] = min;
         vals_max[i_in] = max;
     };
@@ -177,6 +273,7 @@ class MinMaxScaler :  public ScalerFunction {
 
     virtual mlpdouble Distance(const std::vector<mlpdouble> scalar_dim) const 
     {
+        /* Returns positive value if value lies outside range. */
         mlpdouble val_dist{0};
         for (auto iDim=0u; iDim<vals_min.size(); iDim++) {
             if ((scalar_dim[iDim] < vals_min[iDim]) || (scalar_dim[iDim] > vals_max[iDim])){
