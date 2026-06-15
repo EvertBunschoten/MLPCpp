a4189cebfdc2746125d7d7290798f35ea2b46347
root <root@LAPTOP-V2328TE8.localdomain>
2026-06-15 12:54:10 +0300

    added CAdam unit tests and create an independent CMakeLists for UnitTests

--- build/UnitTests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/SU2/subprojects/MLPCpp/UnitTests
-# Build directory: /root/SU2/subprojects/MLPCpp/build/UnitTests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_adam "/root/SU2/subprojects/MLPCpp/build/UnitTests/test_adam")
-set_tests_properties(test_adam PROPERTIES  _BACKTRACE_TRIPLES "/root/SU2/subprojects/MLPCpp/UnitTests/CMakeLists.txt;18;add_test;/root/SU2/subprojects/MLPCpp/UnitTests/CMakeLists.txt;0;")
